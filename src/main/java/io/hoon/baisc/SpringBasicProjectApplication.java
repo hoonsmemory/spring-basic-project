8a08efb353d3d35e1c7096d5a87d3652aa44f8c9
hoon <dltjdgnsok1@naver.com>
2022-05-09 20:38:26 +0900

    Initial commit

---
+++ src/main/java/io/hoon/baisc/SpringBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package io.hoon.baisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hoon/baisc/SpringBasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hoon.baisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
