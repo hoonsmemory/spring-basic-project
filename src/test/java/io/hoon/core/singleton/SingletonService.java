4295e46308a50e8c8fecc1a33ac9d9882d0fbdcd
hoon <dltjdgnsok1@naver.com>
2022-05-17 22:41:10 +0900

    싱글톤 패턴 적용

---
+++ src/test/java/io/hoon/core/singleton/SingletonService.java
@@ -0,0 +1,14 @@
+package io.hoon.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    private SingletonService() {
+
+    }
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+}
