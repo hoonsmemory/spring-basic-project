a71728371e110bd7bc0e16b2952ff578cee5f4e4
hoon <dltjdgnsok1@naver.com>
2022-05-24 23:20:45 +0900

    빈 조회 후 사용

---
+++ src/test/java/io/hoon/core/autowired/allbean/AllBeanTest.java
@@ -0,0 +1,49 @@
+package io.hoon.core.autowired.allbean;
+
+import io.hoon.config.AppConfig;
+import io.hoon.discount.DiscountPolicy;
+import io.hoon.member.Grade;
+import io.hoon.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class, DiscountService.class);
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "discountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policyList;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+            this.policyMap = policyMap;
+            this.policyList = policyList;
+
+            System.out.println("policyMap : " + policyMap);
+            System.out.println("policyList : " + policyList);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+
+            return discountPolicy.discount(member,price);
+        }
+    }
+}
